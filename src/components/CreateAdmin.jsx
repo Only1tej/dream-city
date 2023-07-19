8fc452cd85cea36aef540eb8d7d2f0f7afb00040
only1tej <tijanienny3@gmail.com>
2023-07-19 15:17:27 -0700

    Change of design

--- src/components/CreateAdmin.jsx
+++ src/components/CreateAdmin.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import logo from '../../src/dreamcity/DCAN_logo_sm.jpg'
+import logo from '../../src/dreamcity/Logo.png'
 
 function CreateAdmin() {
     const navigate = useNavigate()
@@ -9,49 +9,51 @@ function CreateAdmin() {
     }
     return (
         <>
-            <div className="avatar flex justify-center items-end mt-4">
-                <div className="w-24 rounded ">
-                    <img src={logo} />
-                </div>
-            </div>
-            <div className="hero min-h-screen bg-white">
-                <div className="hero-content flex-col">
-                    <div className="text-center">
-                        <h1 className="text-4xl font-bold">Create Admin</h1>
+            <div>
+                <div className='flex'>
+                    <div className='flex justify-center items-center w-1/2'>
+                        <img src={logo} alt="Logo" className='' />
                     </div>
-                    <div className="card w-72 h-[530px] md:w-[450px] md:h-[770px] lg:w-[600px] lg:h-[380px] flex-shrink-0 min-w-sm shadow-2xl bg-base-100">
-                        <div className="card-body">
-                            <div className='lg:flex lg:space-x-16 '>
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">First Name</span>
-                                    </label>
-                                    <input type="text" placeholder="Enter first name" className="input input-bordered" />
-                                </div>
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">Last Name</span>
-                                    </label>
-                                    <input type="text" placeholder="Enter last name" className="input input-bordered" />
-                                </div>
+                    <div className="hero min-h-screen bg-[#F5E0B8] w-1/2">
+                        <div className="hero-content flex-col">
+                            <div className="text-left">
+                                <h1 className="text-4xl font-bold text-[#008F97]">Create Admin</h1>
                             </div>
-                            <div className='lg:flex lg:space-x-16 '>
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">Email</span>
-                                    </label>
-                                    <input type="text" placeholder="Enter email" className="input input-bordered" />
+                            <div className="w-72 h-[530px] md:w-[500px] md:h-[470px] lg:w-[600px] lg:h-[380px] flex-shrink-0 min-w-sm bg-base-100">
+                                <div className="bg-[#F5E0B8]">
+                                    <div className='lg:flex lg:space-x-16 '>
+                                        <div className="form-control">
+                                            <label className="label">
+                                                <span className="label-text text-[#F48222]">First Name</span>
+                                            </label>
+                                            <input type="text" placeholder="Enter first name" className="input bg-[#F5E0B8] border-5 border-[#ACABAB] valid:text-[#118286] focus:border-[#ACABAB] focus:outline-none focus:bg-[#F5E0B8]" />
+                                        </div>
+                                        <div className="form-control">
+                                            <label className="label">
+                                                <span className="label-text text-[#F48222]">Last Name</span>
+                                            </label>
+                                            <input type="text" placeholder="Enter last name" className="input bg-[#F5E0B8] border-5 border-[#ACABAB] valid:text-[#118286] focus:border-[#ACABAB] focus:outline-none focus:bg-[#F5E0B8]" />
+                                        </div>
+                                    </div>
+                                    <div className='lg:flex lg:space-x-16 '>
+                                        <div className="form-control">
+                                            <label className="label">
+                                                <span className="label-text text-[#F48222]">Email</span>
+                                            </label>
+                                            <input type="text" placeholder="Enter email" className="input bg-[#F5E0B8] border-5 border-[#ACABAB] valid:text-[#118286] focus:border-[#ACABAB] focus:outline-none focus:bg-[#F5E0B8]" />
+                                        </div>
+                                        <div className="form-control">
+                                            <label className="label">
+                                                <span className="label-text text-[#F48222]">Phone Number</span>
+                                            </label>
+                                            <input type="text" placeholder="Enter phone number" className="input bg-[#F5E0B8] border-5 border-[#ACABAB] valid:text-[#118286] focus:border-[#ACABAB] focus:outline-none focus:bg-[#F5E0B8]" />
+                                        </div>
+                                    </div>
+                                    <div className="form-control mt-6 space-y-3">
+                                        <button className="btn bg-[#118286] outline-none border-none hover:bg-[#118286]">Create Admin</button>
+                                        <button onClick={handleLogin} className='btn bg-[#F5E0B8] hover:bg-[#F5E0B8] border-none text-[#118286]'>Login</button>
+                                    </div>
                                 </div>
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">Phone Number</span>
-                                    </label>
-                                    <input type="text" placeholder="Enter phone number" className="input input-bordered" />
-                                </div>
-                            </div>
-                            <div className="form-control mt-6 space-y-3">
-                                <button className="btn btn-primary">Sign Up</button>
-                                <button onClick={handleLogin} className='btn btn-ghost'>Login</button>
                             </div>
                         </div>
                     </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import logo from '../../src/dreamcity/DCAN_logo_sm.jpg'
+import logo from '../../src/dreamcity/Logo.png'
 
 function Home() {
     const navigate = useNavigate()
@@ -11,36 +11,36 @@ function Home() {
     return (
         <>
             <div>
-                <div className="flex justify-center items-end mt-4">
-                    <div className="w-24 rounded ">
-                        <img src={logo} />
+                <div className='flex'>
+                    <div className='flex justify-center items-center w-1/2'>
+                        <img src={logo} alt="Logo" />
                     </div>
-                </div>
-                <div className="hero min-h-screen bg-white">
-                    <div className="hero-content flex-col">
-                        <div className="text-center">
-                            <h1 className="text-4xl font-bold">Login</h1>
-                        </div>
-                        <div className="card w-72 md:w-[420px] lg:w-[560px] flex-shrink-0 min-w-sm shadow-2xl bg-base-100">
-                            <div className="card-body">
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">Email</span>
-                                    </label>
-                                    <input type="text" placeholder="Enter email" className="input input-bordered" />
-                                </div>
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">Password</span>
-                                    </label>
-                                    <input type="password" placeholder="Enter password" className="input input-bordered" />
-                                    <label className="label">
-                                        <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                                    </label>
-                                </div>
-                                <div className="form-control mt-6 space-y-3">
-                                    <button className="btn btn-primary">Login</button>
-                                    <button onClick={createAdmin} className='btn btn-ghost'>Create Admin</button>
+                    <div className="hero min-h-screen bg-[#F5E0B8] p-0 w-1/2">
+                        <div className="hero-content flex-col">
+                            <div className="text-right">
+                                <h1 className="text-4xl font-bold text-[#008F97]">Login</h1>
+                            </div>
+                            <div className="w-72 md:w-[420px] lg:w-[560px] flex-shrink-0 min-w-sm bg-base-100">
+                                <div className="bg-[#F5E0B8]">
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text text-[#F48222]">Email</span>
+                                        </label>
+                                        <input type="text" placeholder="Enter email" className="input bg-[#F5E0B8] border-5 border-[#ACABAB] valid:text-[#118286] focus:border-[#ACABAB] focus:outline-none focus:bg-[#F5E0B8]" />
+                                    </div>
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text text-[#F48222]">Password</span>
+                                        </label>
+                                        <input type="password" placeholder="Enter password" className="input bg-[#F5E0B8] disabled:bg-[#F5E0B8] border-5 border-[#ACABAB] valid:text-[#118286] invalid:text-[#118286] focus:border-[#ACABAB] focus:outline-none focus:bg-[#F5E0B8]" />
+                                        <label className="label">
+                                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                        </label>
+                                    </div>
+                                    <div className="form-control mt-6 space-y-3">
+                                        <button className="btn bg-[#118286] outline-none border-none hover:bg-[#118286]">Login</button>
+                                        <button onClick={createAdmin} className='btn bg-[#F5E0B8] hover:bg-[#F5E0B8] border-none text-[#118286]'>Create Admin</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>

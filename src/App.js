6b23515e7a29f0c0d76fb56a23467559a008b0e8
only1tej <tijanienny3@gmail.com>
2023-07-21 10:31:20 -0700

    Adding and removing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-import Notyet from "./components/Notyet";
 import Home from "./components/Home";
 import CreateAdmin from "./components/CreateAdmin";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";

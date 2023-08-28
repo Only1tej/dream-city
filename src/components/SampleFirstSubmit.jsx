f513145d5026d23c0aac77609acf4757f58d514b
only1tej <tijanienny3@gmail.com>
2023-08-28 00:55:02 -0700

    Sidebar

--- src/components/SampleFirstSubmit.jsx
+++ src/components/SampleFirstSubmit.jsx
@@ -48,8 +48,8 @@ const SampleFirstSubmit = ({ formData, listings }) => {
     return (
         <>
             {/* <div className='flex flex-row max-h-screen lg:overflow-hidden md:overflow-auto bg-white'> */}
-            <div className='flex flex-row md:flex md:flex-row max-h-screen lg:overflow-hidden md:overflow-auto bg-white' >
-                <div className='w-[315px] h-full min-h-screen bg-[#F5E0B8]'>
+            <div className='flex flex-row md:flex md:flex-row max-h-screen  lg:h-full bg-white ' >
+                <div className='sm:w-[315px] h-full fixed overflow-x-hidden min-h-screen w-[200px] bg-[#F5E0B8]'>
                     <div className='flex flex-col '>
                         <div className='py-4'>
                             <img src={logo} alt="Logo" className='bg-[#F5E0B8] w-1/2 h-1/2 mx-auto' />
@@ -60,13 +60,23 @@ const SampleFirstSubmit = ({ formData, listings }) => {
                         <Link to="/create-listing" className='text-left active:bg-white focus:bg-white text-[#118286] active:text-[#F48222] pl-[31px] py-[16px]'>Create Listing</Link>
                     </div>
                 </div>
-                <div className='mt-[18px] mr-[33px] ml-[19px] mb-[19px]'>
+                <div className='mt-[18px] mr-[33px] ml-[333px] mb-[19px]'>
                     <div>
                         <input type="search" placeholder="Search" className="input input-bordered w-[275px] md:w-[468px] h-[33px] rounded-none bg-white" />
                     </div>
 
-
-                    <div class="grid grid-cols-4 gap-8">
+                    {/* <div className="listings">
+                        <h2>Listings</h2>
+                        <ul>
+                            {listings.map((listing, index) => (
+                                <li key={index}>
+                                    <Link to={`/listing/${index}`}>{listing.title}</Link>
+                                    <Link to={`/listing/${index}`}>{listing.description}</Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </div> */}
+                    <div class="grid lg:grid-cols-4  md:grid-cols-3 gap-x-12 grid-cols-2">
                         {listings.map((listing, index) => (
                             <div className='w-[285px] h-[239px] mt-[40px] mr-[200px]'>
                                 <div className='w-[285px] h-[160px] rounded-t-xl carousel carousel-center'>
@@ -96,7 +106,7 @@ const SampleFirstSubmit = ({ formData, listings }) => {
                                     </Carousel> */}
                                 </div>
                                 <div className=' bg-[#F5E0B8] rounded-b-xl p-1'>
-                                    <p className='text-[#118286] text-sm font-primary font-semibold'>{state?.title}{listing.title}</p>
+                                    <p className='text-[#118286] text-sm font-primary font-semibold'>{listing.title}</p>
                                     <p className='text-[#118286] font-primary font-normal'>{state?.description}{listing.description}</p>
                                     <p className='text-[#118286] text-xs font-primary font-normal'> {state?.location}{listing.location}</p>
                                     <p className='text-[#F48222] text-sm font-primary font-bold'>{state?.landmark}{listing.landmark}</p>
@@ -111,3 +121,4 @@ const SampleFirstSubmit = ({ formData, listings }) => {
 }
 
 export default SampleFirstSubmit
+
